75cfb2891cc149577365ccc944783d70f0d4ad33
huaixiaoz <law.ck@qq.com>
2014-05-29 05:44:44 +0800

    fixed in_file and O_APPEND , delete in_file bin

--- in_file.c
+++ in_file.c
@@ -13,9 +13,11 @@ int main()
     
     int op;
 
-   if((op = open("test.txt", O_RDWR|O_CREAT, S_IRWXU|S_IRWXO|S_IRWXG) == -1)) 
+   if((op = open("test.txt", O_RDWR|O_APPEND|O_CREAT, S_IRUSR|S_IWUSR)) < 0) 
        printf("open error. \n");
-
+#ifdef DEBUG
+   printf("file desciptor : %d\n", op);
+#endif
     while((rtn = read(STDIN_FILENO, buf, BUFSIZE)) != 0)
         /*
          *if(buf == "EOF")
@@ -24,10 +26,13 @@ int main()
          *        exit(0);
          *   }
          */
+    {
+       // printf("###########\t%s\n",buf);
       if((write(op, buf, rtn)) != rtn)
               printf("error write. \n");
+    }
          close(op);     
-
+    
               if(rtn < 0)
               printf("read error.\n");
     exit(0);
