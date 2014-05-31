67b3c54e193bd2fbfc308f30b5de26cd0654f973
huaixiaoz <law.ck@qq.com>
2014-05-31 15:07:06 +0800

    fix fork1 for get argv[1]

--- fork1.c
+++ fork1.c
@@ -4,8 +4,11 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define DEBUG
+//#define DEBUG
 #define BUFSIZE 100
+
+char *n_0(char *st);
+
 int main(int argc, char *argv[])
 {
     
@@ -15,6 +18,8 @@ int main(int argc, char *argv[])
     char *r;
     char buf[BUFSIZE]; 
     int *status;
+    
+    char *exe;
 
     if((pid = fork()) == 0)
     switch(argc)
@@ -29,7 +34,8 @@ int main(int argc, char *argv[])
 #ifdef DEBUG
             printf("fgets().\n");
 #endif
-            buf[strlen(buf)-1] = 0;
+            buf[strlen(buf)-1] = '\0';
+            
             if((execlp(buf, buf, (char *)0)) == -1)          
                 {
                     printf("exec error!\t%s\n",buf);
@@ -38,15 +44,33 @@ int main(int argc, char *argv[])
 #ifdef DEBUG
             printf("here!!\n");
 #endif
+            break;
         case 2:
+            char *ex;
+            ex = n_0(argv[1]);
+            if((execlp(ex, ex, (char *)0))==-1)
+            {
+                printf("execvp error!!\n");
                 exit(0);
+            }
+            break;
     }
         
-    if ((waitpid(pid, status, 0)) < 0)
+    if ((waitpid(-1, status, 0)) != pid)
     {
-        printf("waitpid error .\n");
+        printf("waitpid error .\n%s\n",status);
         exit(0);
     }
     exit(0);
+}
+
+char *n_0(char *st)
+{
+    char *buf;
+    buf = st;
+    while(*st++ != '\n')
+    ;
+    *st = 0;
+    return buf;
 }
 
